bc342933d6ee1ef035ca5db721cadca7e7ecf244
Vladimir <v.yaroshenko@techflower.io>
2025-02-25 14:11:17 +0200

    added didChangeDependencies for crypto_coin

--- lib/screens/crypto_coin_screen.dart
+++ lib/screens/crypto_coin_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
-class CryptoCoinScreen extends StatelessWidget {
+class CryptoCoinScreen extends StatefulWidget {
   const CryptoCoinScreen({super.key});
 
   @override
+  State<CryptoCoinScreen> createState() => _CryptoCoinScreenState();
+}
+
+class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
+  String? coinName;
+
+  @override
+  void didChangeDependencies() {
+    final args = ModalRoute.of(context)?.settings.arguments;
+
+    if (args is String?) {
+      coinName = args;
+    } else {
+      coinName = null;
+    }
+    setState(() {});
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Bitcoin')));
+    return Scaffold(appBar: AppBar(title: Text('$coinName')));
   }
 }
