2747f1d534406e3ff54a934b3b6727651114f273
Vladimir <v.yaroshenko@techflower.io>
2025-03-06 19:44:53 +0200

    fix cripto_coin_details_event

--- lib/features/crypto_coin/bloc/crypto_coin_details_event.dart
+++ lib/features/crypto_coin/bloc/crypto_coin_details_event.dart
@@ -1,18 +1,30 @@
 part of 'crypto_coin_details_bloc.dart';
 
 @immutable
-abstract class CryptoCoinDetailsEvent extends Equatable {
-  const CryptoCoinDetailsEvent();
-
-  @override
-  List<Object> get props => [];
-}
+sealed class CryptoCoinDetailsEvent extends Equatable {}
 
 class LoadCryptoCoinDetails extends CryptoCoinDetailsEvent {
-  const LoadCryptoCoinDetails({required this.currencyCode});
+  LoadCryptoCoinDetails({required this.currencyCode});
 
   final String currencyCode;
 
   @override
-  List<Object> get props => super.props..add(currencyCode);
+  List<Object> get props => [currencyCode];
 }
+// Старий підхід створення
+// @immutable
+// abstract class CryptoCoinDetailsEvent extends Equatable {
+//   const CryptoCoinDetailsEvent();
+
+//   @override
+//   List<Object> get props => [];
+// }
+
+// class LoadCryptoCoinDetails extends CryptoCoinDetailsEvent {
+//   const LoadCryptoCoinDetails({required this.currencyCode});
+
+//   final String currencyCode;
+
+//   @override
+//   List<Object> get props => super.props..add(currencyCode);
+// }
