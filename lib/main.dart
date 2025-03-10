770c3453fd6b03470061c3ba33ae1b2afd482382
Vladimir <v.yaroshenko@techflower.io>
2025-03-10 19:19:57 +0200

    added talker_bloc_logger

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'dart:async';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:crypto_coins_list/crypto_currencies_list_app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger_observer.dart';
 import 'package:talker_dio_logger/talker_dio_logger_interceptor.dart';
 import 'package:talker_dio_logger/talker_dio_logger_settings.dart';
 import 'package:talker_flutter/talker_flutter.dart';
@@ -20,6 +23,14 @@ void main() {
     TalkerDioLogger(
       talker: talker,
       settings: const TalkerDioLoggerSettings(printResponseData: false),
+    ),
+  );
+
+  Bloc.observer = TalkerBlocObserver(
+    talker: talker,
+    settings: const TalkerBlocLoggerSettings(
+      printStateFullData: false,
+      printEventFullData: false,
     ),
   );
 
